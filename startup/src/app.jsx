89aeef6db670a6b56ab9a246d7ec5b6722030005
sethfrand <sethp.frandsen@gmail.com>
2026-02-11 19:28:01 -0700

    Added Routing and updated components

--- startup/src/app.jsx
+++ startup/src/app.jsx
@@ -2,21 +2,38 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+
+
+import {BrowserRouter as Router, Routes, Route, NavLink, BrowserRouter} from "react-router-dom";
+import Overview from './overview/overview';
+import Expenses from './expenses/expenses';
+import Sheets from './sheets/sheets';
+import Login from './login/login';
+
+
 export default function App() {
-    return <div className='body bg-dark text-light'>
+    return (
+    <BrowserRouter>
+    <div className='body bg-dark text-light'>
         <header className="container-fluid bg-dark text-white text-center py-3 position-relative">
 
             <nav className="navbar fixed-top navbar-dark bg-dark px-3">
                 <ul className="navbar-nav flex-row gap-3 ms-auto">
-                    <li className="nav-item"><a className="nav-link" href="../overview/Overview.html">Overview</a></li>
-                    <li className="nav-item"><a className="nav-link" href="../expenses/expenses.html">Expenses</a></li>
-                    <li className="nav-item"><a className="nav-link" href="../sheets/sheets.html">All Sheets</a></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="Overview">Overview</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="Expenses">Expenses</NavLink></li>
+                    <li className="nav-item"><NavLink className="nav-link" to="Sheets">All Sheets</NavLink></li>
                 </ul>
             </nav>
         </header>
 
+        <Routes>
+            <Route path="/Login" element={<Login/>} exact/>
+            <Route path="/Overview" element={<Overview/>} exact/>
+            <Route path="/Expenses" element={<Expenses/>} exact/>
+            <Route path="/Sheets" element={<Sheets/>} exact/>
+            <Route path="/*" element={<NotFound/>}/>
+        </Routes>
 
-        <main className="container-fluid bg-dark text-white text-center">Components will go here</main>
 
         <footer className="bg-dark text-white text-white-70">
             <div className="container-fluid">
@@ -25,4 +42,10 @@ export default function App() {
             </div>
         </footer>
     </div>
+    </BrowserRouter>);
+}
+function NotFound() {
+    return (
+        <div>404 Error Page not found</div>
+    );
 }
---
+++ startup/src/login/login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Login(){
+    return(
+        <main className="container-fluid bg-secondary text-center">
+            <div>login displayed</div>
+        </main>
+    );
+}
