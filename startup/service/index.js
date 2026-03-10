2e8611fc880c918af9dde33c6df00036d6e046b4
sethfrand <sethp.frandsen@gmail.com>
2026-03-09 19:55:24 -0600

    deleted auth.js, figured it would be eaiser to keep it all in one file. Took the scaffolding from simon to build the api requests and functions.

--- startup/service/index.js
+++ startup/service/index.js
@@ -1,15 +1,138 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
 const express = require('express');
+const uuid = require('uuid');
 const app = express();
-const authRouter = require('./routes/auth');
+
+const authCookieName = 'token';
 
 
+let users = [];
+let sheets = [];
+let expenses = []
+
+// The service port. In production the front-end code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
 app.use(express.json());
-app.use(express.static('../public'));
-app.use(require('cookie-parser')());
-app.use('/api/auth',authRouter)
+
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the front-end static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+        res.status(409).send({ msg: 'Existing user' });
+    } else {
+        const user = await createUser(req.body.email, req.body.password);
+
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+    }
+});
+
+// GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('email', req.body.email);
+    if (user) {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            user.token = uuid.v4();
+            setAuthCookie(res, user.token);
+            res.send({ email: user.email });
+            return;
+        }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth logout a user
+apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+// Middleware to verify that the user is authorized to call an endpoint
+const verifyAuth = async (req, res, next) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+        req.user = user;
+        next();
+    } else {
+        res.status(401).send({ msg: 'Unauthorized' });
+    }
+};
 
 
-app.get('/api/test', (req, res) => res.send('Hello World!'));
+apiRouter.get('/sheets', verifyAuth, (req, res) => {
+    const storedSheets = sheets.filter(s => s.user === req.user.email);
+    res.send(storedSheets);
+});
+apiRouter.post('/sheets', verifyAuth, (req, res) => {
+    const newSheet = { ...req.body, owner: req.user.email};
+    sheets.push(newSheet);
+    res.send(newSheet);
+});
+apiRouter.get('/expenses', verifyAuth, (req, res) => {
+    const storedExpenses = expenses.filter(e => e.sheet === req.user.email);
+    res.send(storedExpenses);
+});
+apiRouter.post('/expenses', verifyAuth, (req, res) => {
+    const newExpense = expenses.new(e => e.sheet === req.user.email, req.body);
+    res.send(newExpense);
+});
 
-const port = 4000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Default error handler
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
+async function createUser(email, password) {
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    const user = {
+        email: email,
+        password: passwordHash,
+        token: uuid.v4(),
+    };
+    users.push(user);
+
+    return user;
+}
+
+async function findUser(field, value) {
+    if (!value) return null;
+
+    return users.find((u) => u[field] === value);
+}
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+        maxAge: 1000 * 60 * 60 * 24 * 365,
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+    });
+}
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
