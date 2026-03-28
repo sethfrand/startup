03386f80b9f3ce7c78e342049cad3fce1a580621
sethfrand <sethp.frandsen@gmail.com>
2026-03-28 17:45:19 -0600

    Added connection testing

--- startup/database.js
+++ startup/database.js
@@ -5,4 +5,17 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 const client = new MongoClient(url);
 const db = client.db('fiancesheet');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
+const expenseCollection = db.collection('expenses');
+const sheetCollection = db.collection('sheet');
+
+
+// Testing connection to database on startup, exit if fail
+(async function testConnection() {
+    try {
+        await db.command({ ping: 1 });
+        console.log(`Connect to database`);
+    } catch (ex) {
+        console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+        process.exit(1);
+    }
+})();
